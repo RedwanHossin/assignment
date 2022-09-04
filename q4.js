089f9194752285105997cf53fcab4a1433b54883
RedwanHossin <redwanmredha@gmail.com>
2022-09-04 19:50:26 +0600

    q4

---
+++ q4.js
@@ -0,0 +1,19 @@
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+console.log("Add:", add(1, 2));
+console.log("Subtract:", subtract(1, 2));
+console.log("Multiply:", multiply(1, 2));
+console.log("Divide:", divide(1, 2));
